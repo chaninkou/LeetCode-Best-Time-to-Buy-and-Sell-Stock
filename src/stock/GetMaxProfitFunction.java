748179d779bed5a620d2a8948c646e00d0849983
chaninkou <michaelkoulove@yahoo.com>
2019-12-29 00:50:01 -0800

    Great review on Kadane's Algorithm, cool pattern to do.

--- src/stock/GetMaxProfitFunction.java
+++ src/stock/GetMaxProfitFunction.java
@@ -1,12 +1,15 @@
 package stock;
 
 public class GetMaxProfitFunction {
+	// Kadane's Algorithm
     public int maxProfit(int[] prices) {
         int maxCurrent = 0;
         int maxFinal = 0;
         
+        // Starts from 1 since i - i-1
         for(int i = 1; i < prices.length; i++){
             // Keeping track of profit from current element to previous element
+        	// Cool trick, in the example, maxcurrent will be 0,4,2,5 since it will keep getting the positive number
             maxCurrent = Math.max(0, maxCurrent += prices[i] - prices[i-1]);
             
             // Get the best profit
